8ed9aca0be4156d442c9af26302139c7063dfcb3
iravinderr <imravinderr@gmail.com>
2024-02-21 16:08:53 +0530

    announcements

--- backend/routes/annRoutes.js
+++ backend/routes/annRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {createAnn} = require('../controllers/announcements/createAnn');
+const {getAnn} = require('../controllers/announcements/getAnn');
 
 
 router.post('/createnew/create', createAnn);
+router.get('/retrieve/:id', getAnn);
 
 module.exports = router;
