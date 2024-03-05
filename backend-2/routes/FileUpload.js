3bdd4b4ce389cb4026c5055b72739b07b8bb3439
iravinderr <imravinderr@gmail.com>
2024-03-05 11:35:44 +0530

    backend-2 removed

--- backend-2/routes/FileUpload.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { imageUpload } = require("../controllers/fileUpload");
-
-// Define the post route with a callback function
-router.post("/imageUpload", imageUpload);
-
-module.exports = router;
